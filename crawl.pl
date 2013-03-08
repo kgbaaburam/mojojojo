898dd59326e97b1bca99a5bdb8657f087f63095a
Pamplemousse <pamplemousse@W>
2013-03-08 13:24:09 +0100

    ajout package

--- crawl.pl
+++ crawl.pl
@@ -1,6 +1,9 @@
+package crawl;
+
 use Mojolicious::Lite;
 use Mojo::UserAgent;
 use URI::Escape;
+use Mojo::DOM;
 
 my $ua = Mojo::UserAgent->new;
 
@@ -13,17 +16,34 @@ my $date = uri_escape("08/03/2013");
 my $url = "http://www.covoiturage.fr/recherche?fc=".$depart."&tc=".$arrivee."&d=".$date."&to=BOTH&p=1&n=20&t=tripsearch&a=searchtrip";
 
 
-
 my $contenu = $ua->get($url)->res->dom('li.one-trip');
 
-# my $contenu = $ua->get($url)->res->dom->html->body;
+my @datas;
+
+$contenu->each(sub {
+    my ($e, $count) = @_;
+
+    my $dom = Mojo::DOM->new($e);
 
 
-#   say $ua->get('mojolicio.us')->res->dom->html->head->title->text;
+    # my $lieu_depart;
+    # my $lieu_arrivee;
+    # my $prix;
+    # my $date;
+    # my $heure;
+    # my $duree;
+
+    my $lieux = $dom->at('h2 span');
+
+    # my @data = ($lieu_depart, $lieu_arrivee, $prix, $date, $heure, $duree);
+    push(@datas, $lieux);
+  });
 
 
-get '/' => {text => $contenu};
 
+get '/' => {text => @datas[1]};
 
 
 app->start;
+
+1;
